5ef8d7419a964ed7835394bc214e7f0167de2472
Lê Vũ Hoàng <2251120349@ut.edu.vn>
2025-01-09 17:58:05 +0700

    variable for layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -13,5 +13,4 @@ const Layout = () => {
     </div>
   );
 };
-
 export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
@@ -6,7 +7,22 @@ export default {
   ],
   theme: {
     extend: {
-
+        color:{
+            'primary': 'var(--color-primary)',
+            'secondary': 'var(--color-secondary)',
+            'background': 'var(--bg-color)',
+            'text': 'var(--text-color)',
+        },
+        fontFamily: {
+          'title' : 'var(--font-title)',
+          'mini_title' : 'var(--font-mini-title)',
+          'text': 'var(--font-text)',
+        },
+        fontSize: {
+            'title': 'var(--font-size-title)',
+            'mini_title': 'var(--font-size-mini-title)',
+            'text': 'var(--font-size-text)',
+        }
     },
   },
   plugins: [],
