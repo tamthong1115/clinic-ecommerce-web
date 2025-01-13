6dbcb3d2c755258700fc201510e4a2b04bf4e9ac
caonhathao <73047910+caonhathao@users.noreply.github.com>
2025-01-13 09:33:18 +0700

    Created footer component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+/// <reference types="vite-plugin-svgr/client" />
+
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), svgr({
+        svgrOptions: {
+            exportType: 'named',
+            ref: true,
+            svgo: false,
+            titleProp: true
+        },
+        include: '**/*.svg',
+    })],
 })
