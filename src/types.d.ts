917e4e0cfd76923990a3bc5ea94ab63c0cfc96e6
Truong Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2025-03-05 18:12:14 +0700

    Revert "Created footer component"

--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite-plugin-svgr/client" />
-
-declare module "*.svg" {
-    import { FunctionComponent, SVGProps } from 'react';
-    export const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string }>;
-    const src: string;
-    export default src;
-}
