740c5c8a11b0ea913fcb16546895c3415def39fc
caonhathao <73047910+caonhathao@users.noreply.github.com>
2025-04-16 14:47:25 +0700

    Update and fix - Optimize Header and Navigator bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import AppRoutes from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from './context/ToastContext.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
+
 const queryClient = new QueryClient();
 
 function App() {
+  console.log(import.meta.env.VITE_API_BASE_URL);
   return (
     <QueryClientProvider client={queryClient}>
       <ToastProvider>
--- src/components/Header/NavigatorBar.tsx
+++ src/components/Header/NavigatorBar.tsx
@@ -1,19 +1,24 @@
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import { BsList, BsXLg } from 'react-icons/bs';
 import { navLink } from '../../constants/header/navLink';
-import { useState } from 'react';
-import { BsList } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
 import { AnimatePresence, motion, MotionConfig } from 'framer-motion';
+import logo from '../../assets/logo.png';
+import PublicPaths from '../../routes/public/pathPublic.ts';
+import { useNavigate } from 'react-router-dom';
 
-const NavigatorBar = () => {
+const NavigatorBar = ({ widthDevice }: { widthDevice: number }) => {
   const [activeTab, setActiveTab] = useState(new Array(4).fill(false));
   const [currentTab, setCurrentTab] = useState(-1);
   const [isShowMenu, setIsShowMenu] = useState(false);
+  const [widthOffset, setWidthOffset] = useState('0');
+  const navigate = useNavigate();
 
   const handleEnter = (index: number) => {
     // Need to reformat this code
     setActiveTab((prev) => {
       const newContent = [...prev];
-      if (currentTab === -1) {
+      if (currentTab < 0) {
         setCurrentTab(index);
         newContent[index] = !newContent[index];
       } else if (currentTab !== index) {
@@ -21,6 +26,7 @@ const NavigatorBar = () => {
         newContent[currentTab] = !newContent[currentTab];
         newContent[index] = !newContent[index];
       } else {
+        setCurrentTab(-1);
         newContent[index] = !newContent[index];
       }
       return newContent;
@@ -38,7 +44,14 @@ const NavigatorBar = () => {
     setIsShowMenu(true);
   };
 
-  const getMotionConfig = (isClosed: boolean, distance: string) => {
+  const handleNavigate = (path: string) => {
+    if (path) {
+      setIsShowMenu(false);
+      navigate(path);
+    }
+  };
+
+  const getMotionMobileConfig = (isClosed: boolean, distance: string) => {
     return {
       initial: {
         left: isClosed ? distance : 0,
@@ -60,10 +73,21 @@ const NavigatorBar = () => {
   // useEffect(() => {
   //   // console.log(isShowMenu);
   // }, [isShowMenu]);
+
+  useEffect(() => {
+    if (widthDevice <= 640) {
+      setWidthOffset('-350px');
+    } else setWidthOffset('0');
+  }, [widthDevice]);
+
   return (
-    <div className={'w-full h-max relative'}>
+    <div
+      className={
+        'w-full h-max sm:flex sm:flex-col sm:justify-center sm:items-center'
+      }
+    >
       <div
-        className={`w-fit h-fit p-2 bg-emerald-600 cursor-pointer ${isShowMenu ? `hidden` : `block`}`}
+        className={`w-fit h-fit p-2 sm:bg-emerald-600 cursor-pointer sm:hidden`}
         onClick={handleShowMenu}
       >
         <BsList color={'white'} size={25} />
@@ -71,20 +95,81 @@ const NavigatorBar = () => {
       <MotionConfig>
         <AnimatePresence initial={false}>
           <motion.div
-            {...getMotionConfig(isShowMenu, '-200px')}
-            className=" bg-white top-0 w-fit h-max absolute shadow-lg shadow-gray-500 text-black z-[10]"
+            {...getMotionMobileConfig(isShowMenu, widthOffset)}
+            className=" bg-white top-0 max-w-[80%] sm:w-full h-max absolute sm:relative shadow-lg sm:shadow-none shadow-gray-500 text-black z-[10]"
           >
-            <ul className=" w-full h-full flex flex-col justify-between items-start">
+            <ul className=" w-full h-full flex flex-col sm:flex-row justify-between items-start sm:items-center">
+              {/*This is for brand's banner*/}
+              {widthOffset !== '0' ? (
+                <li
+                  className={
+                    'w-full shadow-md shadow-gray-600 flex flex-row justify-between items-center z-[2]'
+                  }
+                >
+                  <div
+                    className={
+                      'w-[80%] mr-5 flex flex-row justify-start items-center'
+                    }
+                  >
+                    <img src={logo} alt={'logo'} className={'h-20'} />
+                    <div className="w-full px-2 text-blue-800 font-bold">
+                      4ChillyGuys
+                    </div>
+                  </div>
+                  <div className={'w-[20%] ml-5 flex flex-row justify-center'}>
+                    <button type={'button'} onClick={handleCloseMenu}>
+                      <BsXLg />
+                    </button>
+                  </div>
+                </li>
+              ) : null}
+              {/*This is for notify if guest does not sign-in/sign-up*/}
+              {widthOffset !== '0' ? (
+                <li
+                  className={
+                    'w-full bg-gradient-to-tl from-emerald-600 to-emerald-400 p-2'
+                  }
+                >
+                  <div
+                    className={
+                      ' w-full text-left break-words text-white font-bold'
+                    }
+                  >
+                    Đăng nhập để hưởng những đặc quyền dành riêng cho thành viên
+                  </div>
+                  <div className={'mt-2'}>
+                    {/*Buttons for sign-in and sign-up*/}
+                    <button
+                      type={'button'}
+                      className={
+                        'w-fit p-1 rounded-full bg-white text-emerald-600 font-bold mr-3'
+                      }
+                      onClick={() => handleNavigate(PublicPaths.LOGIN)}
+                    >
+                      Đăng nhập
+                    </button>
+                    <button
+                      type={'button'}
+                      className={
+                        'w-fit px-2 py-1 rounded-full bg-emerald-600 text-white font-bold ml-3'
+                      }
+                      onClick={() => handleNavigate(PublicPaths.SIGN_UP)}
+                    >
+                      Đăng kí
+                    </button>
+                  </div>
+                </li>
+              ) : null}
               {navLink.map((item, index) => (
                 <li
-                  className={`w-full font-[500] flex flex-col justify-between items-center gap-2 text-md mb-3 relative cursor-pointer`}
+                  className={`w-full font-[500] flex flex-col justify-between items-center gap-2 text-md mb-3 sm:m-2 relative cursor-pointer`}
                   onClick={() => {
                     handleEnter(index);
                   }}
                   key={index}
                 >
                   <div
-                    className={`w-full p-2 flex flex-row items-center justify-between  ${activeTab[index] ? `bg-emerald-600 text-white` : `bg-white text-black`}`}
+                    className={`w-full p-2 flex flex-row items-center justify-between  ${activeTab[index] ? `text-emerald-600` : `text-black`}`}
                   >
                     {item.nameLink}
                     {activeTab[index] ? (
@@ -99,7 +184,7 @@ const NavigatorBar = () => {
                   </div>
 
                   {activeTab[index] ? (
-                    <ul className="bg-[white] text-[black] w-fit ml-5">
+                    <ul className="bg-[white] text-[black] w-fit ml-5 sm:absolute sm:top-10 sm:shadow-md sm:shadow-gray-500 sm:w-full">
                       {Object.entries(item.item).map(
                         ([key, value], subIndex) => (
                           <li
@@ -118,13 +203,15 @@ const NavigatorBar = () => {
             </ul>
           </motion.div>
 
-          <motion.div
-            {...getMotionConfig(isShowMenu, '-100vw')}
-            className={
-              'w-screen h-screen absolute top-0 left-0 bg-[#5d5d5d4d] z-[1] cursor-pointer'
-            }
-            onClick={handleCloseMenu}
-          ></motion.div>
+          {widthOffset !== '0' ? (
+            <motion.div
+              {...getMotionMobileConfig(isShowMenu, '-600vw')}
+              className={
+                'w-screen h-screen absolute top-0 left-0 bg-[#5d5d5d4d] z-[1] cursor-pointer'
+              }
+              onClick={handleCloseMenu}
+            ></motion.div>
+          ) : null}
         </AnimatePresence>
       </MotionConfig>
     </div>
